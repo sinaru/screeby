8c8af60eadbb6fef509d4ee9f3ec8b766f6a7800
Sinaru Gunawardena <dubdev@lonelyplanet.com>
2020-05-14 17:57:08 +0100

    Server code

---
+++ screeby/server.py
@@ -0,0 +1,36 @@
+from socketserver import BaseRequestHandler, TCPServer
+
+class ClientMessage:
+    def __init__(self, message):
+        self.message_parts = message.decode().split('|')
+        self.message_type = self.message_parts[0]
+
+    def type(self):
+        return self.message_type
+
+class ServerRequestHandler(BaseRequestHandler):
+    def handle(self):
+        print(f"Connected with: {self.client_address}")
+        msg = self.request.recv(8192)
+        if not msg:
+            print(f"Connection closed with: {self.client_address}")
+
+        msg = ClientMessage(msg)
+
+        if msg.type() == 'SERVER_INFO':
+            self.send_server_info()
+
+    def send_server_info(self):
+        self.send_str("RESOLUTION:1366x768")
+
+    def send_str(self, text):
+        self.request.send(text.encode())
+
+class Server:
+    def __init__(self, port):
+        TCPServer.allow_reuse_address = True
+        self.serve = TCPServer(('', port), ServerRequestHandler)
+
+    def run(self):
+        self.serve.serve_forever()
+
