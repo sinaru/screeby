0d839652f0ed39b3bcb23a2fa549466949e4431c
Sinaru Gunawardena <dubdev@lonelyplanet.com>
2020-05-15 23:44:54 +0100

    Try to smooth mouse movement

--- screeby/server/mouse_receiver.py
+++ screeby/server/mouse_receiver.py
@@ -10,24 +10,29 @@ class MouseReceiver:
 
     def run(self):
         while True:
-            data = self.request.recv(32)
+            data = self.request.recv(1024)
             if not data:
                 break
 
-            message = data.decode()
-            key, *event_data = message.split('|')
+            for d in self.chunks(data, 32):
+                message = d.decode()
+                key, *event_data = message.split('|')
 
-            if key == 'move':
-                x, y = event_data[0], event_data[1]
-                self.mouse.position = (int(x), int(y))
+                if key == 'move':
+                    x, y = event_data[0], event_data[1]
+                    self.mouse.position = (int(x), int(y))
 
-            if key == 'click':
-                pressed = True if event_data[1] == 'true' else False
-                name = event_data[0]
-                if pressed:
-                    self.mouse.press(Button[name])
-                else:
-                    self.mouse.release(Button[name])
+                if key == 'click':
+                    pressed = True if event_data[1] == 'true' else False
+                    name = event_data[0]
+                    if pressed:
+                        self.mouse.press(Button[name])
+                    else:
+                        self.mouse.release(Button[name])
 
-            if self.logger: self.logger.info(f"mouse {message}")
+                if self.logger: self.logger.info(f"mouse {message}")
             sleep(self.delay)
+
+    def chunks(self, lst, n):
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
