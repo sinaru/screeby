079ae8562f418d1dcee65c883c088c9cf50ee998
Sinaru Gunawardena <dubdev@lonelyplanet.com>
2020-05-18 22:49:47 +0100

    Fix test

--- tests/server.py
+++ tests/server.py
@@ -3,14 +3,14 @@ import unittest
 
 class ServerTestCase(unittest.TestCase):
     def test__init__method(self):
-        with patch('socketserver.TCPServer') as mock:
-            from screeby.server import Server, ServerRequestHandler
+        with patch('socketserver.ThreadingTCPServer') as mock:
+            from screeby.server.server import Server, ServerRequestHandler
             Server(5555)
             mock.assert_called_once_with(('', 5555), ServerRequestHandler)
 
-    @patch('socketserver.TCPServer')
+    @patch('socketserver.ThreadingTCPServer')
     def test_run_method(self, _mock):
-        from screeby.server import Server
+        from screeby.server.server import Server
         server = Server(5555)
         server.run()
         server.serve.serve_forever.assert_called_once()
